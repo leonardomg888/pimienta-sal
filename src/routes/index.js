dedd12df5693dd2763585e8bca35a224bf5f1166
leonardomg888 <leonardomg888@gmail.com>
2022-08-10 02:22:29 +0200

    listo

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+const {index,detail} = require('../controllers/indexC')
+/* GET home page. */
+
+router
+.get('/', index) 
+.get('/detail/:id',detail);
+
+
+module.exports = router ;
